4edea698a069b3e1f3454deb394d2394347eb802
soasme <soasme@gmail.com>
2013-04-21 22:34:07 +0800

    Add mad spec

--- lib/mad/mad.rb
+++ lib/mad/mad.rb
@@ -2,4 +2,8 @@
 
 class MadTemplate
 
+  def render template, &block
+    'word'
+  end
+
 end
--- spec/mad_spec.rb
+++ spec/mad_spec.rb
@@ -3,6 +3,16 @@
 require 'ruby_quiz'
 
 describe MadTemplate do
-  it "should render word success" do
+
+  before do
+    @mad = MadTemplate.new
   end
+
+  it "should render word success" do
+    result = @mad.render('((word))') do
+      word = 'word'
+    end
+    expect(result).to eq('word')
+  end
+
 end
