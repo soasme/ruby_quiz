4e9141fceb8aada7ccc49363817525ea9554a18f
soasme <soasme@gmail.com>
2013-04-22 22:27:33 +0800

    MadTemplate can render simple words

--- lib/mad/mad.rb
+++ lib/mad/mad.rb
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
 
+require 'erb'
+
 class MadTemplate
 
-  def render template, &block
-    'word'
+  def initialize
+    @mad_variables = Hash.new
+  end
+
+  def render template, local={}
+    local.each do | k, v |
+      @mad_variables[k] = v
+    end
+    erb_template(template).result binding
+  end
+
+  def erb_template template
+    ERB.new transform(template)
+  end
+
+  def transform template
+    template.gsub(/\(\(\s*(.+?)\s*\)\)/, "<%= binding_variable('\\1') %>")
+  end
+
+  def binding_variable word
+    @mad_variables[word]
   end
 
 end
--- spec/mad_spec.rb
+++ spec/mad_spec.rb
@@ -9,10 +9,23 @@ describe MadTemplate do
   end
 
   it "should render word success" do
-    result = @mad.render('((word))') do
-      word = 'word'
-    end
+    result = @mad.render '((word))', {'word'=>'word'}
     expect(result).to eq('word')
+  end
+
+  it "should render nothing in dummy word" do
+    result = @mad.render '(( ))', {}
+    expect(result).to eq('')
+  end
+
+  it "should render word with space success" do
+    result = @mad.render '((  word  ))', {'word'=>'word'}
+    expect(result).to eq('word')
+  end
+
+  it "should render duplex words success" do
+    result = @mad.render '((duplex words))', {'duplex words'=>'duplex_words'}
+    expect(result).to eq('duplex_words')
   end
 
 end
